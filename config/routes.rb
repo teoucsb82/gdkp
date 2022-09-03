ee8a44cbe13951b7f214a32699428d5fa7ea391e
Teo Dell'Amico <teo.dellamico@gmail.com>
2022-09-03 15:28:03 -0700

    Added discord omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
   root 'pages#home'
 end
